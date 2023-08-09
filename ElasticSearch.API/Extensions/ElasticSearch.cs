fde4de95a6b497dd6f98aefbcac0a3133791887c
Dogukan Demir <dogukan.demir991@gmail.com>
2023-08-09 10:08:33 +0300

    Initial Commit

---
+++ ElasticSearch.API/Extensions/ElasticSearch.cs
@@ -0,0 +1,15 @@
+using Elasticsearch.Net;
+using Nest;
+
+namespace ElasticSearch.API.Extensions;
+
+public static class ElasticSearch
+{
+    public static void AddElasticService(this IServiceCollection services, IConfiguration configuration)
+    {
+        var pool = new SingleNodeConnectionPool(new Uri(configuration.GetSection("Elastic")["Url"]!));
+        var settings = new ConnectionSettings(pool);
+        var client = new ElasticClient(settings);
+        services.AddSingleton(client); 
+    }
+}
---
+++ ElasticSearch.API/Models/Product.cs
@@ -0,0 +1,14 @@
+using Nest;
+
+namespace ElasticSearch.API.Models;
+
+public class Product
+{
+    [PropertyName("_id")] public string Id { get; set; } = null;
+    public string Name { get; set; } = null;
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime? Updated { get; set; }
+    public ProductFeature? Feature { get; set; }
+}
